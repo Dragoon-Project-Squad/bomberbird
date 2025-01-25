e31d2d38f185a69d938d2ecab3f2a2d50f57f321
Patcheresu <patcheresu@gmail.com>
2025-01-25 13:02:41 -0500

    powerups synched

--- scripts/breakable.gd
+++ scripts/breakable.gd
@@ -1,6 +1,32 @@
 extends CharacterBody2D
 @onready var breakable_sfx_player := $BreakableSound
+const PICKUP_ENABLED := true
+const PICKUP_SPAWN_BASE_CHANCE := 1.0
 
+func decide_pickup_spawn() -> bool:
+	if !PICKUP_ENABLED:
+		return false
+	var rng = RandomNumberGenerator.new()
+	var rng_result = rng.randf_range(0.0,1.0)
+	if rng_result <= PICKUP_SPAWN_BASE_CHANCE:
+		return true
+	else:
+		return false
+		
+func decide_pickup_type() -> String:
+	var pickup_type
+	var rng = RandomNumberGenerator.new()
+	var rng_result = rng.randi_range(1,3)
+	# TODO: Invent fun spawn table that has chances for different pickups
+	match rng_result:
+		1: pickup_type = "explosion_boost"
+		2: pickup_type = "max_explosion"
+		3: pickup_type = "speed_boost"
+		4: pickup_type = "punch_ability"
+		5: pickup_type = "extra_bomb"
+		_: pickup_type = "explosion_boost"
+	return pickup_type
+	
 @rpc("call_local")
 func exploded(_by_who):
 	#breakable_sfx_player.play()
@@ -8,6 +34,8 @@ func exploded(_by_who):
 	$"AnimationPlayer".play("explode")
 	# Spawn a powerup where this rock used to be.
 	if is_multiplayer_authority():
-		get_node("/root/World/PickupSpawner").spawn(self.position)
+		if decide_pickup_spawn():
+			var type_of_pickup = decide_pickup_type()
+			get_node("/root/World/PickupSpawner").spawn({"spawnpoint": self.position, "pickuptype": type_of_pickup})
 	queue_free()
 	await $"AnimationPlayer".animation_finished #Wait for the animation to finish
