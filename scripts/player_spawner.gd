a015fade7044bdb297a8d4986b469da49720537d
Patcheresu <patcheresu@gmail.com>
2025-01-25 12:40:23 -0500

    removed a print debug statement

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -15,13 +15,13 @@ func _spawn_player(data) -> CharacterBody2D:
 		#$"../Players".add_child(spawningplayer)
 		spawningplayer.synced_position = data.spawndata
 		spawningplayer.name = str(data.pid)
-		print("Data: ",data)
-		print("Multiplayer Unique ID: ", multiplayer.get_unique_id())
-		if data.pid == multiplayer.get_unique_id():
-			print("Chosen Name: ", data.defaultname)
-		elif (data.pid == 1):
-			print("Derived Name: ", data.defaultname)
-		else:
-			print("Derived Name: ", data.playerdictionary[data.pid])
+		#print("Data: ",data)
+		#print("Multiplayer Unique ID: ", multiplayer.get_unique_id())
+		#if data.pid == multiplayer.get_unique_id():
+			#print("Chosen Name: ", data.defaultname)
+		#elif (data.pid == 1):
+			#print("Derived Name: ", data.defaultname)
+		#else:
+			#print("Derived Name: ", data.playerdictionary[data.pid])
 		spawningplayer.set_player_name(data.defaultname if data.pid == multiplayer.get_unique_id() || data.pid == 1 else data.playerdictionary[data.pid])
 		return spawningplayer
