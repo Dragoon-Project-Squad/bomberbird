8abac64d703e85c2b80bfba3f4231ad383f72d39
Patcheresu <patcheresu@gmail.com>
2025-02-09 14:26:29 -0500

    unbreakable functions and fixes

--- scripts/unbreakable.gd
+++ scripts/unbreakable.gd
@@ -1,8 +1,10 @@
-extends CharacterBody2D
+extends Area2D
 @onready var unbreakable_sfx_player := $UnbreakableSound
 
+func _ready():
+	$AnimationPlayer.play("slam")
 
 func _on_body_entered(body: Node2D) -> void:
 	if is_multiplayer_authority() && body.has_method("exploded"):
 		## Explode only on authority.
-		body.exploded.rpc(0)
+		body.exploded.rpc(gamestate.ENVIRONMENTAL_KILL_PLAYER_ID)
