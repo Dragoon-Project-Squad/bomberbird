16990f4d9e108a073fe8db66d8063a3715eef87f
SpoopyTim <spoopytimbusiness@gmail.com>
2025-02-10 13:50:05 +0000

    Fixed collisions of unbreakables spawned by the unbreakable spawner and added properly timed player and object death

--- scripts/unbreakable.gd
+++ scripts/unbreakable.gd
@@ -2,9 +2,26 @@ extends Area2D
 @onready var unbreakable_sfx_player := $UnbreakableSound
 
 func _ready():
+	self.body_entered.connect(_on_body_entered)
+	# Connect a function to make effects only apply on fully fallen unbreakables
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+	# Make sure the bounding collider is disabled
+	$SolidCollider/Shape.disabled = true
 	$AnimationPlayer.play("slam")
 
 func _on_body_entered(body: Node2D) -> void:
+	### Explode only on authority.
 	if is_multiplayer_authority() && body.has_method("exploded"):
-		## Explode only on authority.
-		body.exploded.rpc(gamestate.ENVIRONMENTAL_KILL_PLAYER_ID)
+		var world = get_tree().get_root().get_node("World")
+		var floor: TileMapLayer = world.get_node("Floor")
+		# Check if body is on same tile
+		if floor.map_to_local(floor.local_to_map(body.position)) == floor.map_to_local(floor.local_to_map(self.position)):
+			body.exploded.rpc(gamestate.ENVIRONMENTAL_KILL_PLAYER_ID)
+			
+func _on_animation_finished(name: String):
+	# Enable area collider to check if they're under the unbreakable
+	$AreaCollider.disabled = false
+	# Prevent being pushed before killed
+	await get_tree().create_timer(1).timeout
+	# Enable bounding collider to prevent walking into unbreakable
+	$SolidCollider/Shape.disabled = false
