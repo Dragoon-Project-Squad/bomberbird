bac52b5f1b1c7fc787ae55def6f1134319371624
Patcheresu <patcheresu@gmail.com>
2025-02-10 10:50:07 -0500

    rewrote collisions, signals, and anim for solidwalls

--- scripts/unbreakable.gd
+++ scripts/unbreakable.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 @onready var unbreakable_sfx_player := $UnbreakableSound
 
 func _ready():
@@ -6,7 +6,7 @@ func _ready():
 	#$SolidCollider/Shape.disabled = true
 	$AnimationPlayer.play("slam")
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_hit_detection_body_entered(body: Node2D) -> void:
 	### Explode only on authority.
 	if is_multiplayer_authority() && body.has_method("exploded"):
 		var world = get_tree().get_root().get_node("World")
@@ -15,10 +15,8 @@ func _on_body_entered(body: Node2D) -> void:
 		if floor.map_to_local(floor.local_to_map(body.position)) == floor.map_to_local(floor.local_to_map(self.position)):
 			body.exploded.rpc(gamestate.ENVIRONMENTAL_KILL_PLAYER_ID)
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	# Enable area collider to check if they're under the unbreakable
-	$AreaCollider.disabled = false
-	# Prevent being pushed before killed
-	await get_tree().create_timer(1).timeout
-	# Enable bounding collider to prevent walking into unbreakable
-	#$SolidCollider/Shape.disabled = false
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	## Enable area collider to check if they're under the unbreakable
+	#$AreaCollider.disabled = false
+	## Prevent being pushed before killed
+	await get_tree().create_timer(0.05).timeout
