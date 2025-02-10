38235eaa7f42f495f6714fc7d0a122d58a8e0f15
Patcheresu <patcheresu@gmail.com>
2025-02-10 09:35:10 -0500

    fixed breakable spawner having the wrong script

--- scenes/unbreakable_spawner.gd
+++ scenes/unbreakable_spawner.gd
@@ -1,10 +1,10 @@
 extends MultiplayerSpawner
-const BREAKABLE_SCENE_PATH : String = "res://scenes/unbreakable.tscn"
+const UNBREAKABLE_SCENE_PATH : String = "res://scenes/unbreakable.tscn"
 
 func _init():
 	spawn_function = place_unbreakable
 	
 func place_unbreakable(spawncoords: Vector2):
-	var unbreakable = preload(BREAKABLE_SCENE_PATH).instantiate()
+	var unbreakable = preload(UNBREAKABLE_SCENE_PATH).instantiate()
 	unbreakable.position = spawncoords
 	return unbreakable
