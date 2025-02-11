eb95c51b5fef794ce683ff98d1433339c1bc99ff
Patcheresu <patcheresu@gmail.com>
2025-02-11 18:08:34 -0500

    made hurry up start at 60s
    also moved breakable spawner script to scripts

---
+++ scripts/breakable_spawner.gd
@@ -0,0 +1,13 @@
+extends MultiplayerSpawner
+const BREAKABLE_SCENE_PATH : String = "res://scenes/breakable.tscn"
+@onready var breakable_parent = $"../Breakables"
+
+func _init():
+	spawn_function = place_breakable
+	
+func place_breakable(spawncoords: Vector2):
+	#breakable_layer.set_cell(Vector2i(x,y), BREAKABLE_TILE_ID, Vector2i(0,0),0)
+	var breakable = preload(BREAKABLE_SCENE_PATH).instantiate()
+	breakable.position = spawncoords
+	#breakable_parent.add_child(breakable)
+	return breakable
