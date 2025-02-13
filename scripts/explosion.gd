82e85a3b801e91f1c8bb15d50666ae7106cd075d
SchWiniX <blackDead36@gmail.com>
2025-02-14 00:54:07 +0100

    fixed animation

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -3,27 +3,32 @@ extends Node2D
 @onready var tilemap = get_node("SpriteTileMap")
 @onready var detection_area = get_node("Area2D")
 
-func set_cell_hori(pos: Vector2i, left: int, right: int):
+var right
+var down
+var left
+var up
+
+func set_cell_hori(pos: Vector2i, left: int, right: int, step: int = 0):
 	if pos == Vector2i.ZERO: return
-	var edge_tile: Vector2i = Vector2i(0, 2)
-	var line_tile: Vector2i = Vector2i(0, 1)
+	var edge_tile: Vector2i = Vector2i(step, 2)
+	var line_tile: Vector2i = Vector2i(step, 1)
 	match pos.x:
 		left:
-			tilemap.set_cell(pos, 0, edge_tile, 3)
+			tilemap.set_cell(pos, 0, edge_tile, 1)
 		right:
 			tilemap.set_cell(pos, 0, edge_tile, 0)
 		_:
 			tilemap.set_cell(pos, 0, line_tile, 0)
 
-func set_cell_vert(pos: Vector2i, up: int, down: int):
+func set_cell_vert(pos: Vector2i, up: int, down: int, step: int = 0):
 	if pos == Vector2i.ZERO: return
-	var edge_tile: Vector2i = Vector2i(0, 2)
-	var line_tile: Vector2i = Vector2i(0, 1)
+	var edge_tile: Vector2i = Vector2i(step, 2)
+	var line_tile: Vector2i = Vector2i(step, 1)
 	match pos.y:
 		up:
-			tilemap.set_cell(pos, 0, edge_tile, 2)
-		down:
 			tilemap.set_cell(pos, 0, edge_tile, 1)
+		down:
+			tilemap.set_cell(pos, 0, edge_tile, 2)
 		_:
 			tilemap.set_cell(pos, 0, line_tile, 1)
 
@@ -36,13 +41,13 @@ func _ready():
 
 @rpc("call_local")
 func init_detonate(right: int, down: int = right, left: int = right, up: int = right):
+	self.right = right
+	self.down = down
+	self.left = left
+	self.up = up
+	
 	tilemap.clear() #There is probably a smarterway to do this then to do it all over all the time but that might get complex fast 
-	tilemap.set_cell(Vector2i.ZERO, 0, Vector2i.ZERO, 0)
-	for x in range(-left, right + 1):
-		set_cell_hori(Vector2i(x, 0), -left, right)
-	for y in range(-up, down + 1):
-		set_cell_vert(Vector2i(0, y), -up, down)
-
+	next_detonate()
 
 	var tile_size: float = tilemap.tile_set.tile_size.x
 	detection_area.get_child(0).shape.a = Vector2(-left * tile_size, 0)
@@ -50,12 +55,24 @@ func init_detonate(right: int, down: int = right, left: int = right, up: int = r
 	detection_area.get_child(1).shape.a = Vector2(0, - up * tile_size)
 	detection_area.get_child(1).shape.b = Vector2(0, down * tile_size)
 
+#Gets called once by init_detonate and then from an animation with increasing steps
+func next_detonate(step: int = 0):
+	tilemap.set_cell(Vector2i.ZERO, 0, Vector2i(step, 0), 0)
+	for x in range(-left, right + 1):
+		set_cell_hori(Vector2i(x, 0), -left, right, step)
+	for y in range(-up, down + 1):
+		set_cell_vert(Vector2i(0, y), -up, down, step)
+
+
 @rpc("call_local")
 func do_detonate():
 	show()
+	$AnimationPlayer.play("boom") #this animation is freaky ngl
 	detection_area.monitoring = true
 	detection_area.get_child(0).set_deferred("disabled", 0)
 	detection_area.get_child(1).set_deferred("disabled", 0)
+	await $AnimationPlayer.animation_finished
+	get_parent().done()
 
 func _on_body_entered(body: Node2D) -> void:
 	if is_multiplayer_authority() && body.has_method("exploded"):
