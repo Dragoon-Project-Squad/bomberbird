10f401386fbf119d9d16884da190cb86ffbe85f1
SchWiniX <blackDead36@gmail.com>
2025-02-16 11:49:19 +0100

    added bomb_throwable to bombRoot

--- scripts/bomb_throwable.gd
+++ scripts/bomb_throwable.gd
@@ -4,7 +4,9 @@ const SPEED = 100
 
 @export var synced_position = Vector2()
 
-var coef: Vector3 #coefficients of the throwing polynomial
+@export var throw_gravity: float
+var throw_gravity_direction: Vector2
+
 var tile_map: TileMapLayer
 var is_thrown: bool = false
 
@@ -20,26 +22,14 @@ func _process(delta: float) -> void:
 	else:
 		#The client updates their progess to the last know one
 		position = synced_position
-	position.x = position.x + SPEED * delta
-	position.y = eval_polynomial(position.x)
 
 #throw calculates and starts a throw operations
-func throw(origin: Vector2, target: Vector2, direction: Vector2i, throwing_angle_tan: float):
+func throw(origin: Vector2, target: Vector2, direction: Vector2i, angle: float):
 	assert(direction == Vector2i.LEFT || direction == Vector2i.RIGHT || direction == Vector2i.UP || direction == Vector2i.DOWN)
 	var corrected_origin = tile_map.map_to_local(tile_map.local_to_map(origin))
 	var corrected_target = tile_map.map_to_local(tile_map.local_to_map(target))
-	assert(corrected_origin == corrected_target) #note: this function may not be numeriacally stable. check and ajust if nessessary after testing
 
-	coef.x = (corrected_origin.y - corrected_target.y) / (corrected_origin.x - corrected_target.x) - throwing_angle_tan
-	coef.x /= 2 * corrected_origin.x - (corrected_origin.x * corrected_origin.x - corrected_target.x * corrected_target.x)
+	gravity_direction.x = -direction.y
+	gravity_direction.y = direction.x #90 deg clockwise rotation
 
-	coef.y = throwing_angle_tan - 2 * corrected_origin.x * coef.x
-
-	coef.z = corrected_origin.y - coef.x * corrected_origin.x * corrected_origin.x - coef.y * corrected_origin.x
-	is_thrown = true
-
-func eval_polynomial(x: float) -> float:
-	return coef.dot(Vector3(x * x, x, 1))
-
-func eval_derivative(x: float) -> float:
-	return coef.dot(Vector3(2*x, 1, 0))
+	
