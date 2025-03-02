89fa00f05ba27dfe019109a8b6dae9932030617f
AlanTellezS <43708959+AlanTellezS@users.noreply.github.com>
2025-03-02 00:48:09 -0600

    Fix for AI Dodge
    AI Dodge fixed, it no longer crashes the game and finds a safe place most of the time (requires further testing)

--- scripts/ai_state/ai_bomb.gd
+++ scripts/ai_state/ai_bomb.gd
@@ -4,4 +4,4 @@ class_name AIBombing
 func _physics_update(_delta : float) -> void:
 	if !aiplayer.stunned && aiplayer.last_bomb_time >= aiplayer.BOMB_RATE:
 		aiplayer.place_bomb()
-	state_changed.emit(self, "Wander")
+	state_changed.emit(self, "Dodge")
