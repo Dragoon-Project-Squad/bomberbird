08dc4d8fd724f85029ea07993acdc3f0c2307f09
Patcheresu <patcheresu@gmail.com>
2025-04-06 18:32:02 -0400

    added dragoon music to game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,16 +2,32 @@ extends Control
 @onready var title_sceen: Node2D = $TitleSceen
 @onready var button_box: VBoxContainer = $ButtonBox
 @onready var options_menu: Control = $OptionsMenu
+@onready var main_menu_music_player: AudioStreamPlayer = $AudioStreamPlayer
+signal options_menu_entered
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ButtonBox/Singleplayer.grab_focus()
-
+	
 func switch_to_options_menu() -> void:
+	hide_main_menu()
 	options_menu.visible = true
+	options_menu.options_music_player.play()
+	
+func hide_main_menu() -> void:
 	title_sceen.visible = false
 	button_box.visible = false
 
+func reveal_main_menu() -> void:
+	title_sceen.visible = true
+	button_box.visible = true
+
+func pause_main_menu_music() -> void:
+	main_menu_music_player.stream_paused = true
+	
+func unpause_main_menu_music() -> void:
+	main_menu_music_player.stream_paused = false
+	
 func _on_single_player_pressed() -> void:
 	#if a game is already running do not allow a new game to start
 	if globals.current_world == null:
@@ -24,8 +40,12 @@ func _on_multiplayer_pressed() -> void:
 	if globals.current_world == null:
 		get_tree().change_scene_to_file("res://scenes/lobby.tscn")
 
-
 func _on_options_pressed() -> void:
 	#if a game is already running do not allow to open menu
 	if globals.current_world == null:
+		pause_main_menu_music()
 		switch_to_options_menu()
+
+func _on_options_menu_options_menu_exited() -> void:
+	reveal_main_menu()
+	unpause_main_menu_music()
