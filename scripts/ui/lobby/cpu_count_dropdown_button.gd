db671a75caae838f234af54538031881ae5071f4
Patcheresu <patcheresu@gmail.com>
2025-04-13 15:30:41 -0400

    added getters and setters for battle settings

---
+++ scripts/ui/lobby/cpu_count_dropdown_button.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var dropdown: OptionButton = $HBoxContainer/Dropdown
+
+const CPU_COUNT_DICTIONARY : Dictionary = {
+	"OFF" : "OFF",
+	"ON" : "ON",
+	"SUPER" : "SUPER"
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_cpu_count_items()
+	load_data()
+
+func load_data() -> void:
+	_on_dropdown_item_selected(SettingsContainer.get_resolution_index())
+	dropdown.select(SettingsContainer.get_resolution_index())
+	
+func add_cpu_count_items() -> void:
+	for cpu_count_option in CPU_COUNT_DICTIONARY:
+		dropdown.add_item(cpu_count_option)
+
+func _on_dropdown_item_selected(index: int) -> void:
+	SettingsSignalBus.emit_on_resolution_selected(index)
