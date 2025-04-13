37e2eddf61282cfad01cc0ddfb3b4b9c4100b201
Patcheresu <patcheresu@gmail.com>
2025-04-13 17:59:39 -0400

    made chance slider

--- scripts/ui/lobby/cpu_difficulty_dropdown_button.gd
+++ scripts/ui/lobby/cpu_difficulty_dropdown_button.gd
@@ -2,27 +2,25 @@ extends Control
 
 @onready var dropdown: OptionButton = $HBoxContainer/Dropdown
 
-const CPU_COUNT_DICTIONARY : Dictionary = {
-	"Fill" : "Fill",
-	"0" : "0",
-	"1" : "1",
-	"2" : "2",
-	"3" : "3",
-	"4" : "4"
+const CPU_DIFFICULTY_DICTIONARY : Dictionary = {
+	"Stationary" : "Stationary",
+	"Easy" : "Easy",
+	"Medium" : "Medium",
+	"Hard" : "Hard"
 } 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_cpu_count_items()
+	add_cpu_difficulty_items()
 	load_data()
 
 func load_data() -> void:
 	_on_dropdown_item_selected(SettingsContainer.get_resolution_index())
 	dropdown.select(SettingsContainer.get_resolution_index())
 	
-func add_cpu_count_items() -> void:
-	for cpu_count_option in CPU_COUNT_DICTIONARY:
-		dropdown.add_item(cpu_count_option)
+func add_cpu_difficulty_items() -> void:
+	for cpu_difficulty_option in CPU_DIFFICULTY_DICTIONARY:
+		dropdown.add_item(cpu_difficulty_option)
 
 func _on_dropdown_item_selected(index: int) -> void:
 	SettingsSignalBus.emit_on_resolution_selected(index)
--- scripts/ui/lobby/pickup_spawn_rule_dropdown_button.gd
+++ scripts/ui/lobby/pickup_spawn_rule_dropdown_button.gd
@@ -2,27 +2,25 @@ extends Control
 
 @onready var dropdown: OptionButton = $HBoxContainer/Dropdown
 
-const CPU_COUNT_DICTIONARY : Dictionary = {
-	"Fill" : "Fill",
-	"0" : "0",
-	"1" : "1",
-	"2" : "2",
-	"3" : "3",
-	"4" : "4"
+const PICKUP_SPAWN_RULE_DICTIONARY : Dictionary = {
+	"Stage" : "Stage",
+	"None" : "None",
+	"All" : "All",
+	"Custom" : "Custom"
 } 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_cpu_count_items()
+	add_pickup_spawn_rule_items()
 	load_data()
 
 func load_data() -> void:
 	_on_dropdown_item_selected(SettingsContainer.get_resolution_index())
 	dropdown.select(SettingsContainer.get_resolution_index())
 	
-func add_cpu_count_items() -> void:
-	for cpu_count_option in CPU_COUNT_DICTIONARY:
-		dropdown.add_item(cpu_count_option)
+func add_pickup_spawn_rule_items() -> void:
+	for pickup_spawn_rule_option in PICKUP_SPAWN_RULE_DICTIONARY:
+		dropdown.add_item(pickup_spawn_rule_option)
 
 func _on_dropdown_item_selected(index: int) -> void:
 	SettingsSignalBus.emit_on_resolution_selected(index)
