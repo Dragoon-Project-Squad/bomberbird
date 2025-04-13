04585ee94b3dd016da586e7c12c7e4b611c7640e
Patcheresu <patcheresu@gmail.com>
2025-04-13 19:06:48 -0400

    made checkbox not invisible at first

--- scripts/ui/lobby/hurry_up_state_button.gd
+++ scripts/ui/lobby/hurry_up_state_button.gd
@@ -9,10 +9,18 @@ func _ready() -> void:
 	
 func load_data() -> void:
 	_on_checkbox_toggled(SettingsContainer.get_hurry_up_state())
-
-func _on_checkbox_toggled(toggled_on: bool) -> void:
-	SettingsSignalBus.emit_on_hurry_up_state_set(toggled_on)
-	if toggled_on:
+	set_initial_button_state()
+	
+func set_initial_button_state() -> void:
+	checkbox.button_pressed = SettingsContainer.get_hurry_up_state()
+	set_button_text_to_state(SettingsContainer.get_hurry_up_state())
+	
+func set_button_text_to_state(a_state : bool) -> void:
+	if a_state:
 		checkbox.text = "On"
 	else:
 		checkbox.text = "Off"
+
+func _on_checkbox_toggled(toggled_on: bool) -> void:
+	SettingsSignalBus.emit_on_hurry_up_state_set(toggled_on)
+	set_button_text_to_state(toggled_on)
--- scripts/ui/lobby/sudden_death_state_button.gd
+++ scripts/ui/lobby/sudden_death_state_button.gd
@@ -9,9 +9,16 @@ func _ready() -> void:
 func load_data() -> void:
 	_on_checkbox_toggled(SettingsContainer.get_sudden_death_state())
 
-func _on_checkbox_toggled(toggled_on: bool) -> void:
-	SettingsSignalBus.emit_on_sudden_death_state_set(toggled_on)
-	if toggled_on:
+func set_initial_button_state() -> void:
+	checkbox.button_pressed = SettingsContainer.get_sudden_death_state()
+	set_button_text_to_state(SettingsContainer.get_sudden_death_state())
+	
+func set_button_text_to_state(a_state : bool) -> void:
+	if a_state:
 		checkbox.text = "On"
 	else:
 		checkbox.text = "Off"
+		
+func _on_checkbox_toggled(toggled_on: bool) -> void:
+	SettingsSignalBus.emit_on_sudden_death_state_set(toggled_on)
+	set_button_text_to_state(toggled_on)
