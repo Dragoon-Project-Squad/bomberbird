88609908ff5e6621c27c9eba0362eaf6fdb80f76
SchWiniX <schwinix@proton.me>
2025-05-04 15:16:28 +0200

    detection_handler for type A

---
+++ scenes/enemies/detection_handler_type_A.gd
@@ -0,0 +1,5 @@
+class_name DetectionHandler extends Node2D
+# implements the detection of the player for Priority target 'A' described in https://gamefaqs.gamespot.com/snes/562899-super-bomberman-5/faqs/79457
+
+func check_for_priority_target():
+	return false #Type A is no target hence we always just return false
