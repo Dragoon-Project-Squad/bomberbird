095909f1bbc74435ae585190b1c3281451123671
Rhekar <35650467+Rhekar@users.noreply.github.com>
2025-05-14 17:25:00 -0700

    Add click sound to buttons

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -46,3 +46,7 @@ func _on_options_pressed() -> void:
 func _on_options_menu_options_menu_exited() -> void:
 	reveal_main_menu()
 	unpause_main_menu_music()
+
+
+func _on_singleplayer_pressed() -> void:
+	pass # Replace with function body.
