ce440cdf4aaf49434be4a4479a5970fe1be81462
Patcheresu <patcheresu@gmail.com>
2025-06-07 13:32:09 -0400

    nearly working battle ui

--- scripts/ui/ingame_ui/battlemode_game_ui.gd
+++ scripts/ui/ingame_ui/battlemode_game_ui.gd
@@ -1,79 +1,62 @@
 extends CanvasLayer
 
+@onready var match_timer: Timer = %MatchTimer
+@onready var score_panel_container: Container = %ScorePanelContainer
+@onready var time_label: Label = %TimeLabel
+
+const COLOR_ARR: Array[Color] = [Color8(255, 100, 100), Color8(100, 255, 255), Color8(255, 178, 100), Color8(163, 255, 156)]
+
 var player_labels = {}
 var players_left = -1
 var someone_dead = false
 var time
 
-@onready var match_timer: Timer = %MatchTimer
-@onready var remaining_time: Label = %RemainingTime
-@onready var player_container: HBoxContainer = $Border/Container/Players
+var player_score_panel: Array[ScorePanel]
+var used_player_score_panel_len: int = 0
 
 func _ready() -> void:
 	globals.game.game_ui = self
-	
+	for child in score_panel_container.get_children():
+		if child is ScorePanel:
+			player_score_panel.append(child)
+
+@rpc("call_local")
+func add_player(id: int, _player_name: String, character: String):
+	assert(used_player_score_panel_len <= 3, "attempted to add a 5th player but only 4 are supported")
+	player_score_panel[used_player_score_panel_len].player_id = id
+	player_score_panel[used_player_score_panel_len].show()
+	player_score_panel[used_player_score_panel_len].update_icon(character)
+	player_score_panel[used_player_score_panel_len].update_icon_color(COLOR_ARR[used_player_score_panel_len])
+	player_labels[id] = used_player_score_panel_len
+	used_player_score_panel_len += 1
 
 func start_timer(gametime := SettingsContainer.get_match_time()):
 	match_timer.start(gametime)
-	$Border/Container/Players/Timer/RemainingTime.visible = true
 	
 func stop_timer():
 	match_timer.stop()
-	$Border/Container/Players/Timer/RemainingTime.visible = false
 	
 func _process(_delta: float) -> void:
-	time = match_timer.get_time_left()
-	remaining_time.set_text(time_to_string())
+	time_label.set_text(time_to_string(match_timer.get_time_left()))
 
 func increase_score(playername : String):
-	assert(playername.to_int())
-	var pid : int = playername.to_int()
-	assert(pid in player_labels)
-	var pl = player_labels[pid]
-	pl.score += 1
-	pl.scorelabel.set_text(str(pl.score))
+	var score_panel_slot_num = player_labels[playername]
+	score_panel_container[score_panel_slot_num].increment_score()
 	
 func decrease_score(playername : String):
-	assert(playername.to_int())
-	var pid : int = playername.to_int()
-	assert(pid in player_labels)
-	var pl = player_labels[pid]
-	pl.score -= 1
-	pl.scorelabel.set_text(str(pl.score))
+	var score_panel_slot_num = player_labels[playername]
+	score_panel_container[score_panel_slot_num].decrement_score()
 
 func get_player_score(playername : String):
-	assert(playername.to_int())
-	var pid : int = playername.to_int()
-	assert(pid in player_labels)
-	var pl = player_labels[pid]
-	return pl.score
+	var score_panel_slot_num = player_labels[playername]
+	return score_panel_container[score_panel_slot_num].score
 
-@rpc("call_local")
-func add_player(id: int, new_player_name: String, _character: String):
-	var order: int = player_labels.size() + 1
-
-	player_labels[id] = {
-		namelabel = player_container.get_node("P" + str(order) + "/P" + str(order) + "Name"),
-		scorelabel = player_container.get_node("P" + str(order) + "/P" + str(order) + "Score"),
-		score = 0
-	}
-
-	player_labels[id].namelabel.set_text(new_player_name)
-
-	if order == 1: return
-	player_container.get_node("P" + str(order)).visible = true
-	player_container.get_node("SeparatorP" + str(order - 1) + str(order)).visible = true
-
-func time_to_string() -> String:
+func time_to_string(time := 120.0) -> String:
 	var seconds = fmod(time, 60)
 	var minutes = time / 60
 	var format_string = "%02d:%02d"
 	var actual_time_string = format_string % [minutes, seconds]
 	return actual_time_string
 
-func _on_exit_game_pressed() -> void:
-	gamestate.end_game()
-
-
 func _on_hurry_up_start() -> void:
-	remaining_time.add_theme_color_override("font_color", Color(255, 0, 0)) # Replace with function body.
+	time_label.add_theme_color_override("font_color", Color(255, 0, 0)) # Replace with function body.
--- scripts/ui/ingame_ui/health_panel.gd
+++ scripts/ui/ingame_ui/health_panel.gd
@@ -1,29 +1,7 @@
-class_name HealthPanel extends Control
+class_name HealthPanel extends IconPanel
 
 @onready var health_label: Label = %HealthLabel
-@onready var icon_color_text: TextureRect= %ColoredTexture
-@onready var icon: TextureRect = %Icon
-
-var player_id: int = -1
 
 func update_health(health: int):
 	var format_string = "%01d"
 	health_label.set_text(format_string % [health])
-
-func update_icon(character: String):
-	match character:
-		gamestate.character_texture_paths.EGGOON_PLAYER_TEXTURE_PATH:
-			icon.texture.region = Rect2(192, 96, 32, 32)
-		gamestate.character_texture_paths.NORMALGOON_PLAYER_TEXTURE_PATH:
-			icon.texture.region = Rect2(161, 96, 32, 32)
-		gamestate.character_texture_paths.CHONKGOON_PLAYER_TEXTURE_PATH:
-			icon.texture.region = Rect2(96, 96, 32, 32)
-		gamestate.character_texture_paths.LONGGOON_SELECT_TEXTURE_PATH:
-			icon.texture.region = Rect2(128, 96, 32, 32)
-		gamestate.character_texture_paths.BHDOKI_PLAYER_TEXTURE_PATH:
-			icon.texture.region = Rect2(64, 96, 32, 32)
-		_:
-			icon.texture.region = Rect2(64, 96, 32, 32)
-
-func update_icon_color(color: Color):
-	icon_color_text.modulate = color
