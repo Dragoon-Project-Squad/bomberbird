4b40e19f6f4c2e6c253c087a5ac21d012b5d8ea4
SchWiniX <schwinix@proton.me>
2025-06-28 21:10:35 +0200

    summer_doki_boss_and_breakable_textures_on_runtime

--- scripts/world/zone.gd
+++ scripts/world/zone.gd
@@ -5,8 +5,11 @@ class_name Zone
 @export_group("Zone")
 @export var unbreakable_tile: Vector2i
 @export var tileset_id: int
+@export_enum("saloon", "beach", "dungeon", "lab", "secret") var breakable_texture = 0
 
 func _ready() -> void:
 	_unbreakable_tile = unbreakable_tile
 	_tileset_id = tileset_id
+	_breakable_texture_path = gamestate.obstaclepaths.get_value_by_argument(breakable_texture)
+
 	super()
