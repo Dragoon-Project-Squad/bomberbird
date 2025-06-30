b4b19934a590e80e147180517dbca08fe4e47f78
MonstoBusta <119002070+MonstoBusta@users.noreply.github.com>
2025-06-30 17:47:01 -0500

    Fix to issue 481

--- scripts/music.gd
+++ scripts/music.gd
@@ -3,6 +3,7 @@ extends Node
 func play() -> void:
 	# stops any currently playing music to be safe, then plays battle music
 	Wwise.post_event("stop_music", self)
+	$AkState.set_value()
 	Wwise.post_event("play_music_battle", self)
 
 func stop() -> void:
