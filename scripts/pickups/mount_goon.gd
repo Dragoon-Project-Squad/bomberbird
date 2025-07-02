86832e7c6a56d31d39a32153129187e4c8ea1d22
Patcheresu <patcheresu@gmail.com>
2025-07-02 14:11:56 -0400

    mounted players can't grab mount pickups

--- scripts/pickups/mount_goon.gd
+++ scripts/pickups/mount_goon.gd
@@ -4,10 +4,16 @@ func _ready():
 	super()
 	animated_sprite.play("idle")
 
+func _on_body_entered(body: Node2D) -> void:
+	var is_mounted := false
+	if body is Player:
+		is_mounted = body.is_mounted
+	if not is_mounted:
+		super(body)
+
 func apply_power_up(pickup_owner):
 	super(pickup_owner)
 	if pickup_owner is HumanPlayer: #If this is a human
-		# Increase the bomb level ONLY for the person who obtained it
 		pickup_owner.mount_dragoon.rpc()
 	elif pickup_owner is AIPlayer: #This is an AI
 		pickup_owner.mount_dragoon()
