6ed8253193c84c04097adefe01b64ad40813339b
Patcheresu <patcheresu@gmail.com>
2026-01-24 14:33:38 -0500

    graceful animation stop before queue free

--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -17,6 +17,8 @@ func anim_intro() -> void:
 
 # Called in Main Menu by MenuMusic with no input from player
 func end_intro(intro_ended_via_input : bool = false ) -> void:
+	if animation_player.is_playing():
+		animation_player.stop()
 	get_node("/root/MainMenu").reveal_main_menu()
 	intro_screen_shown.emit(intro_ended_via_input)
 	queue_free()
